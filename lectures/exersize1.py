1d678c74166b7255ab7fd6d4ea2c900d2107fedb
Armen Donigian <donigian@gmail.com>
2016-01-19 21:40:52 -0800

    Week 2 part 1.

---
+++ lectures/exersize1.py
@@ -0,0 +1,10 @@
+# write to file
+print("Hello World!")
+print("Hello Again")
+print("I like typing this.")
+print("This is fun.")
+print('Yay! Printing.')
+print("I'd much rather you 'not'.")
+print('I "said" do not touch this.')
+
+#%lsmagic
